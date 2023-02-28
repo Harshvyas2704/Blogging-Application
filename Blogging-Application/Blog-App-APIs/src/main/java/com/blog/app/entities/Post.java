102cd441541860c11fd2a28998bca78c43b8f023
Harsh Vyas <harshvyasdev@gmail.com>
2023-02-28 19:28:09 +0530

    Comment entity added.

---
+++ Blogging-Application/Blog-App-APIs/src/main/java/com/blog/app/entities/Post.java
@@ -0,0 +1,18 @@
+package com.blog.app.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+	
+	@Id
+	private Integer postId;
+
+}
